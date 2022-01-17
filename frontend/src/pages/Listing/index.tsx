a26386a5c1c518756405ef1aca0cd51805258fe9
rogerplis <rogerplis@hotmail.com>
2022-01-16 21:41:48 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,19 @@
+import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 import React from "react";
+import { BASE_URL } from "utils/requests";
 
 export default function Listing() {
+
+  // errado 
+axios.get(`${BASE_URL}/movies?size=12&page=0`)
+.then(response => {
+  console.log(response.data);
+}
+
+)
+
   return (
     <>
       <Pagination />
