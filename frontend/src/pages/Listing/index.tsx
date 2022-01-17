2f41755b1b93a4fd2ca223fde83001695ae02b23
rogerplis <rogerplis@hotmail.com>
2022-01-16 21:53:34 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,20 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 export default function Listing() {
+  const [pageNumber, setPageNumber] = useState(0);
 
-  // errado 
-axios.get(`${BASE_URL}/movies?size=12&page=0`)
-.then(response => {
-  console.log(response.data);
-}
-
-)
+  useEffect(() => {
+    axios.get(`${BASE_URL}/movies?size=12&page=0`).then((response) => {
+      const data = response.data as MoviePage;
+      setPageNumber(data.number);
+      console.log(response.data);
+    });
+  }, []);
 
   return (
     <>
